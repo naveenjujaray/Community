28473ff506476925a4307219472dec0c2c82089d
naveenjujaray <jujaraynaveen@gmail.com>
2022-10-24 16:46:43 +0530

    Material 3 is true.

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
