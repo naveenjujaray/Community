94a6e7b81c68ac14be222204ff5a7770d4802a45
Sudo2SU <karthik4601@gmail.com>
2022-10-28 22:08:36 +0530

    tree example

---
+++ lib/pages/auth/logout/logout.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Logout extends StatefulWidget {
+  const Logout({super.key});
+
+  @override
+  State<Logout> createState() => _LogoutState();
+}
+
+class _LogoutState extends State<Logout> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
